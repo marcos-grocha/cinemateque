bb67ad4c31df44b2f10be21dc618491a88c6386c
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-12-02 15:08:37 -0300

    Primeiro commit

---
+++ spec/system/actors/user_register_actor_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra ator' do
+  it 'e não está logado' do
+    FactoryBot.create(:user, email: 'joao@email.com', password: '123456789')
+
+    visit new_actor_path
+
+    expect(current_path).to eq new_user_session_path
+  end
+
+  it 'com sucesso' do
+    user = FactoryBot.create(:user, email: 'joao@email.com', password: '123456789')
+    login_as user
+
+    visit root_path
+    within('nav') do
+      click_on 'Atores'
+    end
+    click_on 'Registrar Atores'
+
+    expect(page).to eq 'Vejo já isso'
+  end
+
+  it 'com sucesso e vê detalhes' do
+    
+  end
+
+  it 'com os dados errados' do
+    
+  end
+end
---
+++ spec/system/roles/user_register_role_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra papel' do
+  it 'e não está logado' do
+    
+  end
+
+  it 'com sucesso' do
+    
+  end
+
+  it 'com dados errados' do
+    
+  end
+end
